b076c32a45fa2321683d68337d80811ecb298d96
mimel <millergarciaq@gmail.com>
2019-03-26 16:21:09 -0500

    Landing page ok

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class="full-height" lang="en">
+<?php include 'base/head.html';?>
+<body id="top">
+    <header>
+        <?php include 'base/nav.html';?>
+        <?php include 'base/section.html';?>
+    </header>
+    <div id="content">
+
+        <section class="text-center py-5 grey lighten-4" id="about">
+            <div class="container">
+                <div class="wow fadeIn">
+                    <h2 class="h1 pt-5 pb-3">¿Por que elegirnos?</h2>
+                    <p class="px-5 mb-5 pb-3 lead blue-grey-text">
+                        Somos una concesionaria que compite a nivel nacional con los mejores precios del mercado.
+                    </p>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 mb-r wow fadeInUp" data-wow-delay=".3s"><i class="fa fa-dashboard fa-3x orange-text"></i>
+                        <h3 class="h4 mt-3">Diseño</h3>
+                        <p class="mt-3 blue-grey-text">
+                            El diseño automotriz marca la diferencia.
+                        </p>
+                    </div>
+                    <div class="col-md-4 mb-r wow fadeInUp" data-wow-delay=".4s"><i class="fa fa-comments-o fa-3x cyan-text"></i>
+                        <h3 class="h4 mt-3">Recomendaciones</h3>
+                        <p class="mt-3 blue-grey-text">
+                           somos la concesionaria mas recomendada por nuestros fieles clientes.
+                        </p>
+                    </div>
+                    <div class="col-md-4 mb-r wow fadeInUp" data-wow-delay=".5s"><i class="fa fa-cubes fa-3x red-text"></i>
+                        <h3 class="h4 mt-3">Ejecución</h3>
+                        <p class="mt-3 blue-grey-text">
+                            Todos nuestros tramites se ejecutan en tiempo rapido.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="text-center py-5 indigo darken-1 " id="pricing">
+            <div class="container">
+                <div class="wow fadeIn text-white">
+                    <h2 class="h1 pt-5 pb-3">Nuestros Automoviles</h2>
+                    <p class="px-5 mb-5 pb-3 lead">
+                        Tenemos a su disposición la mejor gama de automoviles.
+                    </p>
+                </div>
+                <!-- Portfolio Gallery Grid -->
+                <div class="row wow fadeIn">
+                    <div class="column col-lg-4 col-md-12 mb-r">
+                        <div class=" wow zoomIn card ">
+                            <img src="img/cars/prado.jpg" alt="Mountains" style="width:100%">
+                            <br/>
+                            <h3>Toyota prado</h3>
+                            <p>Descripcion</p>
+                            <p>$500.000.000</p>
+                            <p><a class="btn btn-outline-black mt-5"> Comprar</a></p>
+                        </div>
+                    </div>
+                    <div class="column col-lg-4 col-md-12 mb-r">
+                        <div class=" wow zoomIn card ">
+                            <img src="img/cars/prado.jpg" alt="Mountains" style="width:100%">
+                            <br/>
+                            <h3>Toyota prado</h3>
+                            <p>Descripcion</p>
+                            <p>$500.000.000</p>
+                            <p><a class="btn btn-outline-black mt-5"> Comprar</a></p>
+                        </div>
+                    </div>
+                    <div class="column col-lg-4 col-md-12 mb-r">
+                        <div class=" wow zoomIn card ">
+                            <img src="img/cars/prado.jpg" alt="Mountains" style="width:100%">
+                            <br/>
+                            <h3>Toyota prado</h3>
+                            <p>Descripcion</p>
+                            <p>$500.000.000</p>
+                            <p><a class="btn btn-outline-black mt-5"> Comprar</a></p>
+                        </div>
+                    </div>
+                    <div class="column col-lg-4 col-md-12 mb-r">
+                        <div class=" wow zoomIn card ">
+                            <img src="img/cars/prado.jpg" alt="Mountains" style="width:100%">
+                            <br/>
+                            <h3>Toyota prado</h3>
+                            <p>Descripcion</p>
+                            <p>$500.000.000</p>
+                            <p><a class="btn btn-outline-black mt-5"> Comprar</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+            </div>
+        </section>
+        <section class="py-5" id="team">
+            <div class="container">
+                <div class="wow fadeIn">
+                    <h2 class="h1 pt-5 pb-3 text-center">Nuestro Equipo</h2>
+                    <p class="px-5 mb-5 pb-3 lead text-center blue-grey-text">
+                        Conformado con los mejores especialista en las areas encargadas para brindar el mejor apoyo.
+                    </p>
+                </div>
+                <div class="row mb-lg-4 center-on-small-only">
+                    <div class="col-lg-6 col-md-12 mb-r wow fadeInLeft" data-wow-delay=".3s">
+                        <div class="col-md-6 float-left"><img class="img-fluid rounded z-depth-1 mb-3" src="img/woman-1.jpg" alt="team member"/></div>
+                        <div class="col-md-6 float-right">
+                            <div class="h4">Nicole West</div>
+                            <h6 class="font-bold blue-grey-text mb-4">Lead Designer</h6>
+                            <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur.</p><a href="https://twitter.com" target="_blank"><i class="fa fa-twitter"></i><span class="ml-1">@nicolewest</span></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 mb-r wow fadeInRight" data-wow-delay=".3s">
+                        <div class="col-md-6 float-left"><img class="img-fluid rounded z-depth-1 mb-3" src="img/woman-2.jpg" alt="team member"/></div>
+                        <div class="col-md-6 float-right">
+                            <div class="h4">Hannah Cruz</div>
+                            <h6 class="font-bold blue-grey-text mb-4">Photographer</h6>
+                            <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur.</p><a href="https://twitter.com" target="_blank"><i class="fa fa-twitter"></i><span class="ml-1">@hannahcruz</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row center-on-small-only">
+                    <div class="col-lg-6 col-md-12 mb-r wow fadeInLeft" data-wow-delay=".3s">
+                        <div class="col-md-6 float-left"><img class="img-fluid rounded z-depth-1 mb-3" src="img/man-1.jpg" alt="team member"/></div>
+                        <div class="col-md-6 float-right">
+                            <div class="h4">Mark Hall</div>
+                            <h6 class="font-bold blue-grey-text mb-4">Web Developer</h6>
+                            <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur.</p><a href="https://twitter.com" target="_blank"><i class="fa fa-twitter"></i><span class="ml-1">@markhall</span></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 mb-r wow fadeInRight" data-wow-delay=".3s">
+                        <div class="col-md-6 float-left"><img class="img-fluid rounded z-depth-1 mb-3" src="img/man-2.jpg" alt="team member"/></div>
+                        <div class="col-md-6 float-right">
+                            <div class="h4">Vincent Harris</div>
+                            <h6 class="font-bold blue-grey-text mb-4">Web Developer</h6>
+                            <p class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic tenetur.</p><a href="https://twitter.com" target="_blank"><i class="fa fa-twitter"></i><span class="ml-1">@vincentharris</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contact" style="background-image:url('img/panorama-3094696_1920.jpg');">
+            <div class="rgba-black-strong py-5">
+                <div class="container">
+                    <div class="wow fadeIn">
+                        <h2 class="h1 text-white pt-5 pb-3 text-center">Contactános</h2>
+                        <p class="text-white px-5 mb-5 pb-3 lead text-center">
+                            Queremos siempre darte la mejor atencion es por eso que si tienes alguna duda escribenos y nos pondremos
+                            de inmediato en contacto con tigo.
+                        </p>
+                    </div>
+                    <div class="card mb-5 wow fadeInUp" data-wow-delay=".4s">
+                        <div class="card-body p-5">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <form action="https://formspree.io/youremail@example.com" method="POST">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="md-form">
+                                                    <input class="form-control" id="name" type="text" name="name" required="required"/>
+                                                    <label for="name">Tu nombre</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="md-form">
+                                                    <input class="form-control" id="email" type="text" name="_replyto" required="required"/>
+                                                    <label for="email">Tu correo</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="md-form">
+                                                    <input class="form-control" id="subject" type="text" name="subject" required="required"/>
+                                                    <label for="subject">Asunto</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="md-form">
+                                                    <textarea class="md-textarea" id="message" name="message" required="required"></textarea>
+                                                    <label for="message">Tu mensaje</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="center-on-small-only mb-4">
+                                            <button class="btn btn-indigo ml-0" type="submit"><i class="fa fa-paper-plane-o mr-2"></i> Enviar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul class="list-unstyled text-center">
+                                        <li class="mt-4"><i class="fa fa-map-marker indigo-text fa-2x"></i>
+                                            <p class="mt-2">Avenida de las Americas 123, torre 2 apto 201, Bogota DC</p>
+                                        </li>
+                                        <li class="mt-4"><i class="fa fa-phone indigo-text fa-2x"></i>
+                                            <p class="mt-2">+ 57 320 311 36 32</p>
+                                        </li>
+                                        <li class="mt-4"><i class="fa fa-envelope indigo-text fa-2x"></i>
+                                            <p class="mt-2">contacto@vendeautosya.com</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section></div>
+    <?php include 'base/footer.html';?>
+    <?php include 'base/js.html';?>
+</body>
+</html>
